8b6cb95f7e853ffafab2e8a27e2dac8153171ee8
yxchan <gnomemage1990@gmail.com>
2023-11-05 18:07:34 +0800

    commit

---
+++ react-kanban/client/src/components/KanbanColumn.tsx
@@ -0,0 +1,68 @@
+import { ReactNode } from "react";
+import {useDroppable} from '@dnd-kit/core';
+import KanBanCard from "./KanbanCard";
+
+export interface ColumnProp {
+  title: string,
+  headerColour: string,
+  cardData: any;
+  columnProps: {
+    id: number,
+  }
+  CardContent: JSX.ElementType
+  openModal: (id: number) => void
+}
+
+const KanBanColumn = (props:ColumnProp) => {
+
+  const { id } = props.columnProps
+  const CardContent = props.CardContent
+
+  return (
+    <>
+      <Droppable id={id.toString()}>
+        <div className="w-[30vw] bg-white rounded-lg shadow-md p-4 flex flex-col">
+          <div>Create new card...</div>
+          <div>{props.cardData.length} cards</div>
+          <h2 className={`text-lg font-semibold mb-4 p-2`}
+          style={{ background: props.headerColour }}>{props.title}</h2>
+          {
+            props.cardData.length > 0 && props.cardData.map((value,index)=>{
+              //console.log('value', value)
+              const { id } = value
+              return <KanBanCard 
+                  key={index}  
+                  id={id}
+                  openModal={props.openModal}
+                >
+                  <CardContent {...value} />
+                </KanBanCard>
+            })
+          }
+        </div>
+      </Droppable>
+    </>
+  );
+};
+
+export default KanBanColumn;
+
+export interface DroppableProp {
+  id: string
+  children: ReactNode
+}
+
+export function Droppable(props:DroppableProp) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: props.id,
+  });
+  const style = {
+    opacity: isOver ? 0.5 : 1,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
--- react-kanban/client/src/pages/Projects.tsx
+++ react-kanban/client/src/pages/Projects.tsx
@@ -3,12 +3,47 @@ import {
     QueryClient,
     QueryClientProvider,
     useQuery,
-  } from 'react-query';
+} from 'react-query';
+import useModal from "../hooks/useModal";
+// import { useQuery } from "@tanstack/react-query";
+import Draggable from '../components/Draggable';
+import Droppable from '../components/Droppable';
+import {
+    DndContext,
+    DragEndEvent,
+    KeyboardSensor,
+    MouseSensor,
+    TouchSensor,
+    useSensor,
+    useSensors,
+  } from '@dnd-kit/core';
+  import { Oval } from "react-loader-spinner";
+  import Column from '../components/KanbanColumn'
   const queryClient = new QueryClient();
 
 export default function Projects() {
 
-    const { data:projects, status, error, isLoading } = useQuery({ 
+    const keyboardSensor = useSensor(KeyboardSensor)
+    const mouseSensor = useSensor(MouseSensor, {
+        // Require the mouse to move by 10 pixels before activating
+        activationConstraint: {
+          distance: 10,
+        },
+      });
+    const touchSensor = useSensor(TouchSensor, {
+        // Press delay of 250ms, with tolerance of 5px of movement
+        activationConstraint: {
+          delay: 250,
+          tolerance: 5,
+        },
+      });
+    const sensors = useSensors(
+        mouseSensor,
+        keyboardSensor,
+        touchSensor
+    );
+
+    const { data:projects, status, isError, error, isLoading } = useQuery({ 
         queryKey: ['projects'],
         queryFn: async() => {
             const res = await fetch("/projects");
@@ -16,13 +51,61 @@ export default function Projects() {
         }
     });
 
-    return (
-    <div className="">
-        {
-            projects && projects.map((value:any,index:number)=>{
-                return <div>{value}</div>
-            })
+    function handleDragEnd(e:DragEndEvent) {
+        console.log("handleDragEnd",e)
+        if (e.over) {
+
         }
-    </div>
+    }
+    
+    const { openModal, Modal } = useModal();
+
+    return (
+        // <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+        //     <Droppable id="droppable">
+        //             <div className="border-2 border-black rounded p-2 inline-block ">
+        //                 <img src="/images/5207497.png" style={{ width:"80px", height:"80px" }} />
+        //             </div>
+        //         </Droppable>
+        // </DndContext>
+        <div className="flex p-8">
+            <Oval
+                height={80}
+                width={80}
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperClass="justify-center z-50 m-auto fixed w-full h-full items-center"
+                visible={isLoading}
+                ariaLabel="oval-loading"
+                secondaryColor="#4fa94d"
+                strokeWidth={2}
+                strokeWidthSecondary={2}
+            />
+                <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+                    <div className="flex space-x-4 bg-gray-100 p-10">
+                        {
+                            isLoading ? 'Loading....' : isError ? "Error...." :                             
+                            projects && projects.map((value:any,index:number)=>{
+                                return <Column 
+                                key={value.id} 
+                                title={value.title} 
+                                // columnProps={value}
+                                // cardData={tasks} 
+                                // headerColour={headerColour[value.id]}
+                                // CardContent={CardContent}
+                                openModal={(id:number)=>{
+
+                                }}
+                            />
+                            })
+                        }
+                    </div>
+                </DndContext>
+                <Modal>
+                    <div className="absolute">
+
+                    </div>
+                </Modal>
+            </div>
     );
 };
